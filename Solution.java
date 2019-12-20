85ce48e955f77126ed086e3e91dfd0a06f0c9fbe
Fox-Red <57450504+Fox-Red@users.noreply.github.com>
2019-12-20 22:41:20 +0800

    Add files via upload
    字符串回文练习，但是出了点问题，明天继续改

--- Solution.java
+++ Solution.java
@@ -1,36 +1,41 @@
-package demo_thread;
+import java.util.ArrayList;
+import java.util.Stack;
 
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-    }
-}
-
-class Solution {
-    public ListNode swapPairs(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        if (head.next == null) {
-            return head;
-        }
-        ListNode newHead = head.next;
-        ListNode prev = null;
-        ListNode cur = head;
-        while (cur != null && cur.next != null) {
-            ListNode next = cur.next.next;
-            cur.next.next = cur;
-            if (prev != null) {
-                prev.next = cur.next;
+public class Solution {
+    public String longestPalindrome(String s) {
+        ArrayList<String> list = new ArrayList<>();
+        Stack<Character> stark = new Stack<>();
+        for (int i = 0;i < s.length(); i++) {
+            int lead = 0;
+            if (stark.isEmpty() || (!stark.peek().equals(s.charAt(i)) && !stark.peek().equals(s.charAt(i + 1)))) {
+                stark.push(s.charAt(i));
+            } else {
+                StringBuilder sb = new StringBuilder(stark.pop());
+                while (stark.peek().equals(s.charAt(i++))) {
+                    sb.append(stark.pop());
+                    sb.append(s.charAt(i));
+                    lead++;
+                }
+                if (lead != 0) {
+                    i--;
+                    list.add(sb.toString());
+                    continue;
+                }
+                while (stark.peek().equals(s.charAt(++i + 1))){
+                    sb.append(s.charAt(i + 1));
+                    sb.append(s.charAt(i));
+                    sb.append(stark.pop());
+                }
+                i--;
+                list.add(sb.toString());
             }
-            cur.next = next;
-            prev = cur;
-            cur = cur.next;
         }
-        return newHead;
+        String str = "";
+        for (int i = 0; i < list.size(); i++) {
+            if (str.length() < list.get(i).length()) {
+                str = list.get(i);
+            }
+        }
+        return str;
     }
 }
